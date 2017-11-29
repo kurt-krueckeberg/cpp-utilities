5a02a88d7392fe8d384a293053d122df48f66ce2
kurt krueckeberm <kurt.krueckeberg@gmail.com>
2017-11-29 09:33:50 -0500

    updates

--- token-iter.h
+++ token-iter.h
@@ -8,7 +8,7 @@ class token_iterator : public std::iterator<std::input_iterator_tag, std::string
     
   bool ok;
   
-  const std::string *const pline; 
+  const std::string *const str_ptr; 
     
   int  pos;
   int  prev_pos;
@@ -17,40 +17,40 @@ class token_iterator : public std::iterator<std::input_iterator_tag, std::string
   
   void fetch_field(bool initial=false) 
   {
-     ok = (pline && pos != pline->size()) ? true : false;
+     ok = (str_ptr && pos != str_ptr->size()) ? true : false;
 
      if (!ok) prev_pos = pos; // This ensures *operator() always returns an empty string if user 
                               // calls operator++() once iteration is complete.
 
      if (ok) {
 
-         if (!initial && pos != pline->size()) ++pos;  // skip over token or advance to start 
+         if (!initial && pos != str_ptr->size()) ++pos;  // skip over token or advance to start 
 
          prev_pos = pos;
 
-         pos = pline->find(token, pos);
+         pos = str_ptr->find(token, pos);
 
          if (pos == std::string::npos) {
              
-             pos = pline->size();
+             pos = str_ptr->size();
          }
      } 
   }
 
   public:
 
-   token_iterator() : token{TOKEN}, ok{false}, pline{nullptr}
+   token_iterator() : token{TOKEN}, ok{false}, str_ptr{nullptr}
    {
        prev_pos = pos = 0;
    } 
 
-   explicit token_iterator(const std::string& in_line) : pline{&in_line}, token{TOKEN}, ok{true}
+   explicit token_iterator(const std::string& in_line) : str_ptr{&in_line}, token{TOKEN}, ok{true}
    {
       prev_pos = pos = 0;
       fetch_field(true);  
    }
 
-   token_iterator(const token_iterator& lhs) : pline{lhs.pline}, token{lhs.token}, pos{lhs.pos}, prev_pos{lhs.pos} {}
+   token_iterator(const token_iterator& lhs) : str_ptr{lhs.str_ptr}, token{lhs.token}, pos{lhs.pos}, prev_pos{lhs.pos} {}
 
   ~token_iterator() {}
   
@@ -73,7 +73,7 @@ class token_iterator : public std::iterator<std::input_iterator_tag, std::string
    {
      auto length = (pos == std::string::npos) ? 0 : pos - prev_pos;
    
-     return pline->substr(prev_pos, length);
+     return str_ptr->substr(prev_pos, length);
    } 
 
    const std::string *operator->()
@@ -83,7 +83,7 @@ class token_iterator : public std::iterator<std::input_iterator_tag, std::string
  
    bool equal(const token_iterator& rhs) const
    {
-     return (ok == rhs.ok) && (!ok || (pline == rhs.pline && pos == rhs.pos && prev_pos == rhs.prev_pos));
+     return (ok == rhs.ok) && (!ok || (str_ptr == rhs.str_ptr && pos == rhs.pos && prev_pos == rhs.prev_pos));
    }
 };
 
